5ee7a33d83d66460f56d9757e1a6bc179c2df968
bloodyxu <tuziibanez@gmail.com>
2016-02-15 00:37:09 +0800

    更新目录结构

--- init.sh
+++ init.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/bash
-
+# 建立目录结构
 mkdir src
-mkdir dist
-mkdir preview
-mkdir sketch-doc
-
 mkdir src/images
 mkdir src/fonts
 mkdir src/styles
-touch src/styles/style.less
 mkdir src/resources
+mkdir dist
 mkdir dist/images
+mkdir preview
 mkdir preview/artboards
+mkdir sketch-doc
+# 建立基本文件
+touch src/styles/style.less
 touch preview/index.html
-
 
 echo '项目结构建立完成'
