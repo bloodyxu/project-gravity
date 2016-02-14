208467d95a242963541352563410189f3e689c89
bloodyxu <tuziibanez@gmail.com>
2016-02-14 17:34:02 +0800

    调整了创建的文件目录

--- init.sh
+++ init.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/bash
 
 mkdir src
+mkdir dist
+mkdir preview
+mkdir sketch-doc
+
 mkdir src/images
 mkdir src/fonts
 mkdir src/styles
 touch src/styles/style.less
 mkdir src/resources
-
-mkdir dist
 mkdir dist/images
-touch dist/index.html
+mkdir preview/artboards
+touch preview/index.html
+
 
 echo '项目结构建立完成'
