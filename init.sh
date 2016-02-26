dde919d95fe88695250d402d1304a94e6c83147a
bloodyxu <tuziibanez@gmail.com>
2016-02-26 15:18:37 +0800

    提交更新

--- init.sh
+++ init.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/bash
-# 建立目录结构
+
 mkdir src
 mkdir src/images
 mkdir src/fonts
 mkdir src/styles
+touch src/styles/style.less
 mkdir src/resources
+
 mkdir dist
 mkdir dist/images
-<<<<<<< HEAD
-cp "./template.html" "./dist/"
-=======
-mkdir preview
-mkdir preview/artboards
-mkdir sketch-doc
-# 建立基本文件
-touch src/styles/style.less
-touch preview/index.html
->>>>>>> bloodyxu/master
+touch dist/index.html
+
+cp "./index.html" "./dist"
 
 echo '项目结构建立完成'
--- install.sh
+++ install.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/bash
-
-# 下载nodejs安装包
-curl -O https://nodejs.org/dist/v4.3.0/node-v4.3.0.pkg
-# 安装nodejs
-sudo installer -store -pkg "./node-v4.3.0.pkg" -target "/"
-
 # 检测是否全局安装Gulp
 if test -e /usr/local/bin/gulp
     then
@@ -16,27 +10,24 @@ if test -e /usr/local/bin/gulp
         # 安装全局的Gulp
         sudo npm install gulp --global
         sudo npm install gulp --save-dev
+
 fi
 
-<<<<<<< HEAD
-echo "开始安装sketchtool"
-cd sketchtool
-sh ./install.sh
-cd ../
-echo "sketchtool安装完毕"
 
-=======
->>>>>>> bloodyxu/master
+
+cp "./sketchtool/bin/sketchmigrate" /usr/local/bin/
+echo Installed `"./sketchtool/bin/sketchmigrate" --version` in "/usr/local/bin"
+
+cp "./sketchtool/bin/sketchtool" /usr/local/bin/
+mkdir -p /usr/local/share/sketchtool
+cp -r "./sketchtool/share/sketchtool/resources.bundle" /usr/local/share/sketchtool/
+echo Installed `"./sketchtool/bin/sketchtool" --version` in "/usr/local/bin"
+
+
 # 安装项目依赖包
-echo "开始安装依赖宝"
 npm install gulp-sketch --save-dev
 npm install gulp-server-livereload --save-dev
 npm install gulp-less --save-dev
 npm install gulp-shell --save-dev
-<<<<<<< HEAD
-npm install gulp-filelist
-echo "工作环境部署完成"
-=======
 
-rm node-v4.3.0.pkg
->>>>>>> bloodyxu/master
+npm install gulp-filelist
